cd17d283c942602337c8d4947bb4e22d67138610
Ashwot-Acharya <ashwot1133@gmail.com>
2021-09-18 00:59:03 +0545

    front page animation design

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,121 @@
-<div class="jumbotron text-center ">
-    <h1><img src="/osiris-rmbg.png" class=" img-fluid" style=" padding:0px; height:100px" alt="main logo"> OSIRIS </h1>
+<div class="bg-secondary" >
+<div class="jumbotron text-center bg-dark ">
+<div class='box'>
+    <div class='wave -one'></div>
+    <div class='wave -two'></div>
+    <div class='wave -three'></div>
+    <img src="/osiris-rmbg.png" class=" img-fluid" style=" padding:0px; height:150px" alt="main logo">
+    <div class="wrapper" > 
+        <div class=" title typing">
+             OSIRIS 
+        </div>
+    </div>
 
 </div>
-<div class="" ></div>
+
+</div>
+</div>
 <style>
-    /* .hero{
-        display: block;
-        box-sizing: border-box;
-        height: 500px;
-        background-color:blue;
-        clip-path: ellipse(150% 100% at 131% 0%);    } */
+
+.wrapper {
+height: 100%;
+/*This part is important for centering*/
+display:flex;
+align-items:center;
+justify-content: center;
+}
+@keyframes typing {
+from { width: 0}
+to {width: 6em;}
+}
+
+@keyframes blink {
+0% {
+border-color: transparent
+
+}
+}
+
+.box {
+    height: 300px;
+    border-radius: 100px;
+    box-shadow: 0 2px 30px rgba(black, .2);
+    background: lighten(#f0f4c3, 10%);
+    position: relative;
+    overflow: hidden;
+    transform: translate3d(0, 0, 0);
+    }
+
+.wave {
+    opacity: .5;
+    position: absolute;
+    top: 0%;
+    left: 60%;
+    background: limegreen;
+    width: 500px;
+    height: 500px;
+    margin-left: -0;
+    margin-top: -200px;
+    transform-origin: 0% 60%;
+    border-radius: 50%;
+    animation: drift 4000ms infinite linear;
+}
+
+.wave.-three {
+    margin-left: -300px;
+    margin-top: -300px;
+    animation: drift 4300ms infinite linear;
+    background-color: lightgreen;
+    border-radius: 50%;
+
+}
+
+.wave.-two {
+    position:absolute;
+    margin-left: 250px;
+    animation: drift 4300ms infinite reverse linear;
+    opacity: .5;
+    border-radius: 50%;
+    background: greenyellow;
+}
+
+
+.box:after {
+    content: '';
+    display: block;
+    left: 0;
+    top: 0;
+    height: 100%;
+    background: linear-gradient(to bottom, rgba(#e8a, 1), rgba(#def, 0) 80%, rgba(white, .5));
+    z-index: 1;
+    transform: translate3d(0, 0, 0);
+}
+
+ .title {
+    width: 30ch;
+    animation: typing 2s steps(30), blink .5s step-end infinite alternate;
+    position: relative;
+    white-space: nowrap;
+    overflow: hidden;
+    border-right: 3px solid;
+    font-family: monospace;
+    font-size: 2em;
+    top: -45%;
+    width: 100%;
+    line-height: 400px;
+    transform: translate3d(0, 0, 0);
+    color: black;
+} 
+@keyframes drift {
+    from { transform: rotate(0deg); }
+    from { transform: rotate(180deg); }
+    from { transform: rotate(360deg); }
+}
 </style>
+
+
 <svelte:head>
-	<title>Home</title>
-	<meta name="robots" content="noindex nofollow" />
-	<html lang="en" />
+<title>Home</title>
+<meta name="robots" content="noindex nofollow" />
+<html lang="en" />
 </svelte:head>
