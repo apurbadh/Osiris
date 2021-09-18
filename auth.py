988dd15ac16606cafa6d99eef240be7822e09d8d
apurbadh <erikduke182@gmail.com>
2021-09-18 11:14:29 +0545

    requirements.txt added

---
+++ auth.py
@@ -0,0 +1,31 @@
+import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+class AuthHandler():
+	security = HTTPBearer()
+	pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+	secret = 'secret_go_brr'
+
+	def get_password_hash(self, password):
+		return self.pwd_context.hash(password)
+
+	def encode_token(self, user_id):
+		payload = {
+			"exp" : datetime.utcnow() + timedelta(days=7),
+			"iat" : datetime.utcnow(),
+			"sub" : user_id
+		}
+		return jwt.encode(payload, self.secret, algorithm="HS256")
+
+	def decode_token(self, token):
+		try:
+			payload = jwt.decode(toke, self.secret, algorithms=["HS256"]
+		except:
+			raise HTTPException(status_code=401, detail="Invalid Token")
+
+	def auth_wrapper(self, auth : HTTPAuthorizationCredentials = Security(security)):
+		return self.decode_token(auth.credentials)
+
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from sqlalchemy.sql.functions import user
 from models import User, Doctor, Base
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from passlib.hash import bcrypt
 from machinelearning import prediction
 from db import SessionLocal, engine
-from  sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from fastapi.middleware.cors import CORSMiddleware
+from auth import AuthHandler
 
 
 Base.metadata.create_all(bind=engine)
 localSession = Session(bind=engine)
 
 app = FastAPI()
+auth_handler = AuthHandler()
 
 origins = ["http://localhost:3000"]
 
@@ -50,18 +52,18 @@ async def login_user(username: str, password:str):
         return {"message" : "Username does not exist"}
     elif not bcrypt.verify(password, found_user.hashed_password):
         return {"message" : "Password does not match"}
-
-    return {"message" : "Successfully logged in"}     
+    token = auth_handler.encode_token(found_user["username"])
+    return {"message" : "Successfully logged in", "token" : token}     
 
 
 @app.get("/api/doctor")
-async def get_doctors(page : int):
+async def get_doctors(page : int, username=Depends(auth_handler.auth_wrapper)):
     latest_data = localSession.query(Doctor).all()[(page - 1) * 10 : page * 10]
     return latest_data
 
 
 @app.get("/api/doctor/{doctor_id}")
-async def get_doctor_by_id(doctor_id : int = Path(None, description="Get individual users information", gt=0)):
+async def get_doctor_by_id(doctor_id : int = Path(None, description="Get individual users information", gt=0), username=Depends(auth_handler.auth_wrapper)):
     data = localSession.query(Doctor).get(doctor_id)
     return data
 
