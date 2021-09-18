a11106e3d1e4ecaad35bd48354056b78ce49c163
Samrat Nakarmi <samratnakarmi1@gmail.com>
2021-09-18 14:20:57 +0545

    temp

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,8 +2,15 @@
 </script>
 
 <div class="container my-3 nav-bar">
+
     <a class="btn btn-success" href="/">About us </a>
     <a class="btn btn-success" href="/login"> Login </a>
     <a class="btn btn-success" href="/Comments">Inbox</a>
     
 </div>
+
+<style>
+    .container{
+
+    }
+</style>
