fdbab994815050857ad05f959c9c5e521b63d4f5
apurbadh <erikduke182@gmail.com>
2021-09-19 18:51:45 +0545

    merge

--- auth.py
+++ auth.py
@@ -3,6 +3,14 @@ from fastapi import HTTPException, Security,Path
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from uuid import UUID
+from pydantic import BaseModel
+
+class SessionData(BaseModel):
+    username: str
+
 
 class AuthHandler():
     security = HTTPBearer()
@@ -29,4 +37,39 @@ class AuthHandler():
 
     def auth_wrapper(self, auth : HTTPAuthorizationCredentials = Security(security)):
         return self.decode_token(auth.credentials)
+
+
+class SessionHandler(SessionVerifier[UUID, SessionData]):
+    def __init__(
+        self,
+        *,
+        identifier: str,
+        auto_error: bool,
+        backend: InMemoryBackend[UUID, SessionData],
+        auth_http_exception: HTTPException,
+    ):
+        self._identifier = identifier
+        self._auto_error = auto_error
+        self._backend = backend
+        self._auth_http_exception = auth_http_exception
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def auto_error(self):
+        return self._auto_error
+
+    @property
+    def auth_http_exception(self):
+        return self._auth_http_exception
+
+    def verify_session(self, model: SessionData) -> bool:
+        """If the session exists, it is valid"""
+        return True
 
--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 from sqlalchemy.sql.functions import user
 from models import User, Doctor, Base, Messages
-from fastapi import FastAPI, Depends,Path
+from fastapi import FastAPI, Depends,Path, Response
 from passlib.hash import bcrypt
 from machinelearning import prediction
 from db import SessionLocal, engine
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from fastapi.middleware.cors import CORSMiddleware
-from auth import AuthHandler
+from auth import AuthHandler, SessionData, SessionHandler
+from uuid import uuid4
+from uuid import UUID
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+from fastapi import HTTPException
 
+cookie_params = CookieParameters()
+
+# Uses UUID
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE",
+    cookie_params=cookie_params,
+)
+
+backend = InMemoryBackend[UUID, SessionData]()
 
 Base.metadata.create_all(bind=engine)
+from pydantic import BaseModel
 
+verifier = SessionHandler(identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),)
 
 app = FastAPI()
 auth_handler = AuthHandler()
@@ -26,8 +48,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/predict")
-async def get_prediction(data: str):
+@app.get("/api/predict",dependencies=[Depends(cookie)])
+async def get_prediction(data: str, session_data: SessionData = Depends(verifier)):
     data = data.split(",")
     ans = prediction(data) 
     return {"prediction" : ans}
@@ -47,7 +69,7 @@ async def register_user(name: str, username:str, email:str, password:str):
 
 
 @app.post("/api/login")
-async def login_user(username: str, password:str):
+async def login_user(username: str, password:str, response : Response):
     localSession = Session(bind=engine)
     found_user = localSession.query(User).filter(User.username == username).first()
     if not found_user:
@@ -55,32 +77,39 @@ async def login_user(username: str, password:str):
     elif not bcrypt.verify(password, found_user.hashed_password):
         return {"message" : "Password does not match"}
     token = auth_handler.encode_token(found_user.username)
+    session = uuid4()
+    data = SessionData(username=username)
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
     return {"message" : "Successfully logged in", "token" : token}     
 
 
-@app.get("/api/doctor")
-async def get_doctors(page : int, username=Depends(auth_handler.auth_wrapper)):
+@app.get("/api/doctor", dependencies=[Depends(cookie)], )
+async def get_doctors(page : int, session_data: SessionData = Depends(verifier)):
     localSession = Session(bind=engine)
     latest_data = localSession.query(Doctor).all()[(page - 1) * 10 : page * 10]
     return latest_data
 
 
-@app.get("/api/doctor/{doctor_id}")
-async def get_doctor_by_id(doctor_id : int = Path(None, description="Get individual users information", gt=0), username=Depends(auth_handler.auth_wrapper)):
+@app.get("/api/doctor/{doctor_id}", dependencies=[Depends(cookie)])
+async def get_doctor_by_id(doctor_id : int = Path(None, description="Get individual users information", gt=0), session_data: SessionData = Depends(verifier)):
     localSession = Session(bind=engine)
     data = localSession.query(Doctor).get(doctor_id)
     return data
 
-@app.post('/api/chat/send')
-async def send_message(sender : int, reciever : int, message : str):
+@app.post('/api/chat/send',dependencies=[Depends(cookie)])
+async def send_message(sender : int, message : str, session_data: SessionData = Depends(verifier)):
     localSession = Session(bind=engine)
+    reciever = localSession.query(User).filter(User.username == session_data.user).first().id
     message = Messages(sender, reciever, message)
     localSession.add(message)
     localSession.commit()
 
 
-@app.get("/api/chat/{sender}/{reciever}")
-async def get_chat(sender : int, reciever):
+@app.get("/api/chat/{sender}/", dependencies=[Depends(cookie)])
+async def get_chat(sender : int, session_data: SessionData = Depends(verifier)):
     localSession = Session(bind=engine)
+    reciever = localSession.query(User).filter(User.username == session_data.user).first().id
     message = localSession.query(Messages).filter(Messages.sender == sender)
     message = message.filter(Messages.reciever == reciever)
+    return message
