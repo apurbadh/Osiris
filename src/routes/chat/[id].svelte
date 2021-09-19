7f916db2e3d07044f064a10c2f54b42e36cc443d
apurbadh <erikduke182@gmail.com>
2021-09-19 19:09:24 +0545

    chat

--- src/routes/chat/[id].svelte
+++ src/routes/chat/[id].svelte
@@ -1,23 +1,25 @@
-<script type="module">
-    export async function load({params}){
-        return {
-            props: {
-                pagename: params.id
-            }
-        }
-    }
-</script>
-
 <script>
-    let pagename;
+
+    let pagename = 1;
+    let mes;
 let messages = []
+const sendMessage = () => {
+    fetch("http://localhost:8000/api/chat/send?message=" + mes, {
+        method: "post"
+    }).then(() => {
+        console.log("sent")
+    })
+}
 setTimeout(() => {
-    fetch("localhost:8000/api/chat/" + pagename).then(res =>res.json()).then(res => messages = res)
+    fetch("http://localhost:8000/api/chat/" + pagename).then(res =>res.json()).then(res => {messages = res
+    console.log(res)})
 }, 2000)
 </script>
-
+  <input bind:value={mes}>
+    <button on:click={sendMessage}>Send</button>
 
 <ul>
+  
 {#each messages as message}
     <li>{message}</li>
 {/each}
