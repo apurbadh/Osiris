0c9a9b4536a26b434816058291fbecce414e9ecc
Ashwot-Acharya <ashwot1133@gmail.com>
2021-09-19 14:45:26 +0545

    touches

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,9 +4,7 @@
                 <div class="col-sm-6 col-md-3 item">
                     <h2>About</h2>
                     <ul>
-                        <li><a href="#">Sentinel</a></li>
-                        <li><a href="#">Team</a></li>
-                        <li><a href="#">Careers</a></li>
+                        <li>Sentinel</li>
                     </ul>
                     
                     <p class="copyright" style="margin-top: 1.5rem;">Sentinel © 2021</p>
@@ -16,12 +14,12 @@
                     <p>  We have only one goal we want to fulfill more than anything, to make a world a better place. Right now, the people everywhere are riddled with questions regarding themselves. Some get an answer while others don't. We took it upon ourselves to help and guide those who did not find their answers and lead them in a healthier life. </p>
                 </div>
                 <div class="col">
-                  <a href="https://github.com/apurbadh"> <img class="avatar" src="https://avatars.githubusercontent.com/u/65907464?v=4" > <i class="icon"></i></a>
-                  <a href="https://github.com/ashshelby"><img class="avatar" src="https://avatars.githubusercontent.com/u/67235783?v=4"> <i class="icon"></i></a>
-                  <a href="https://github.com/prasun004"> <img class="avatar" src="https://avatars.githubusercontent.com/u/75210309?v=4" > <i class="icon"></i></a>
+                  <a href="https://github.com/apurbadh"> <img class="avatar" alt="Apurva" src="https://avatars.githubusercontent.com/u/65907464?v=4" > <i class="icon"></i></a>
+                  <a href="https://github.com/ashshelby"><img class="avatar" alt="Ashutosh" src="https://avatars.githubusercontent.com/u/67235783?v=4"> <i class="icon"></i></a>
+                  <a href="https://github.com/prasun004"> <img class="avatar" alt="Prasun" src="https://avatars.githubusercontent.com/u/75210309?v=4" > <i class="icon"></i></a>
                   <br>
-                  <a href="https://github.com/samrat-nakarmi"> <img class="avatar" style="margin-left: 1.5rem;" src="https://scontent.fbwa1-1.fna.fbcdn.net/v/t1.6435-1/s200x200/137006842_2890755514502832_1290393176386706807_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=TLVXSWPttDUAX99w1ZQ&_nc_ht=scontent.fbwa1-1.fna&oh=eda0655936fb1548553e225a0631fffa&oe=61582C9E"> <i class="icon"></i></a>
-                  <a href="https://github.com/Ashwot-Acharya"> <img class="avatar" src="https://avatars.githubusercontent.com/u/77325112?v=4"> <i class="icon"></i></a>
+                  <a href="https://github.com/samrat-nakarmi"> <img class="avatar" alt="samrat" style="margin-left: 1.5rem;" src="https://scontent.fbwa1-1.fna.fbcdn.net/v/t1.6435-1/s200x200/137006842_2890755514502832_1290393176386706807_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=TLVXSWPttDUAX99w1ZQ&_nc_ht=scontent.fbwa1-1.fna&oh=eda0655936fb1548553e225a0631fffa&oe=61582C9E"> <i class="icon"></i></a>
+                  <a href="https://github.com/Ashwot-Acharya"> <img class="avatar" alt="Ashwot" src="https://avatars.githubusercontent.com/u/77325112?v=4"> <i class="icon"></i></a>
                 </div>
             </div>
 
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -6,7 +6,7 @@
 
     <a class="btn btn-success" href="/login"> Login </a>
     <a class="btn btn-success" href="/inbox">Inbox</a>
-    <a class ="btn btn-success" href="/Doctors"> Avaible Doctors </a>
+    <a class ="btn btn-success" href="/contacts"> Avaible Doctors </a>
 
     
 
