ecb14e36fd6ef463d179f255536055b23c2919c0
Ashwot-Acharya <ashwot1133@gmail.com>
2021-09-19 16:15:50 +0545

    admin panel and cards

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -1,3 +1,27 @@
-<div class="container" >
+<div class="text-center">
+ <div class="jumbotron text-center" >
+     <h1> Admin</h1>
+    <div class="card my-3 text-center bg-info" style="width:300px;">
 
+        <form class="my-3 text-center " >
+            <input type = "gmail"  class="form-contorl" placeholder="example@gmail.com" id="gmail" >
+
+            <input type=" text"  class="form-contorl" id="username" placeholder="Username" >
+            <input type ="password"  class="form-contorl " id="pwd" placeholder="Password" >
+            <br>
+            <button type="submit"  class="btn  btn-primary" on:click="{admin}">Login</button>
+            
+        </form>
+    {#if auth == "some code "} <Administrator/> {:else } enter something {/if}
+    
+    </div>
 </div>
+ </div>
+<script  >
+    import Administrator from "../components/Administrator.svelte";
+   var auth;
+   function admin(){
+     
+   }
+
+</script>
---
+++ src/routes/contacts.svelte
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="card my-3 col card-info" style="width:300px;">
+        <div class="card-header">Header</div>
+        <div class="card-body">Content</div>
+        <div class="card-footer">Footer</div>
+    </div>
+    <div class="card my-3 col card-info" style="width:300px;">
+        <div class="card-header">Header</div>
+        <div class="card-body">Content</div>
+        <div class="card-footer">Footer</div>
+    </div>
+    <div class="card my-3 col card-info" style="width:300px;">
+        <div class="card-header">Header</div>
+        <div class="card-body">Content</div>
+        <div class="card-footer">Footer</div>
+    </div>
+</div>
+
+<svelte:head>
+<title>Avaible Doctors</title>
+<meta name="robots" content="noindex nofollow" />
+<html lang="en" />
+</svelte:head>
